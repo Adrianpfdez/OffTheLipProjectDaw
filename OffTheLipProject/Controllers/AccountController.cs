4b9c6956188f3f43fd7e7485def0b2a1db6ba5ab
Adrian <adriancito46_@hotmail.com>
2019-12-12 12:29:57 +0100

    Redirect Register

--- OffTheLipProject/Controllers/AccountController.cs
+++ OffTheLipProject/Controllers/AccountController.cs
@@ -270,7 +270,7 @@ namespace OffTheLipProject.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirmar cuenta", "Para confirmar la cuenta, haga clic <a href=\"" + callbackUrl + "\">aquí</a>");
 
-                    return RedirectToAction("Index", "Admin");
+                    return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
